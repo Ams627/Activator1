0c48138c9994f731da926d62cd93334479b405c3
Adrian Sims <adriansims62781@gmail.com>
2020-05-19 21:33:44 +0200

    Updates.

--- Activator1/Program.cs
+++ Activator1/Program.cs
@@ -4,16 +4,37 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Activator1
 {
+    class X
+    {
+        public X(XElement e)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    static class ActivatorUtils
+    {
+        public static T CreateInstanceFromTypeName<T>(XElement classInfo, params object[] constructorParams) where T : class
+        {
+            var classname = classInfo?.Attribute("Class")?.Value;
+            var cparams = new object[] { classInfo }.Concat(constructorParams).ToArray();
+            var type = Type.GetType(classname);
+            return Activator.CreateInstance(type, cparams) as T;
+        }
+    }
     class Program
     {
         private static void Main(string[] args)
         {
             try
             {
-                
+                var typename = typeof(X).AssemblyQualifiedName;
+                var elem = new XElement("Ftp", new XAttribute("Class", typename));
+                ActivatorUtils.CreateInstanceFromTypeName<X>(elem);
+                Console.WriteLine();
             }
             catch (Exception ex)
             {
